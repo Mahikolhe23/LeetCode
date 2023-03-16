1fd210f01619eab289200616a02e06aaf101f953
Mahendra Kolhe <mkolhe23@gmail.com>
2023-03-17 00:33:58 +0530

    Added solution - LeetHub

---
+++ Nearly sorted - GFG/nearly-sorted.java
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class gfg
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int num = sc.nextInt();
+            int k = sc.nextInt();
+            
+            int arr[] = new int[num];
+            for(int i = 0; i < num; i++)
+                arr[i] = sc.nextInt();
+            
+            ArrayList <Integer> res = new Solution().nearlySorted(arr, num, k);
+            for (int i = 0; i < res.size (); i++)
+                System.out.print (res.get (i) + " ");
+            System.out.println();
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to return the sorted array.
+    ArrayList <Integer> nearlySorted(int arr[], int num, int k)
+    {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+		PriorityQueue<Integer> qu = new PriorityQueue<Integer>();
+		for (int i = 0; i <= k; i++) {
+			qu.offer(arr[i]);
+		}
+		for (int i = k + 1; i < arr.length; i++) {
+			list.add(qu.poll());
+			qu.add(arr[i]);
+		}
+		while (!qu.isEmpty()) {
+			list.add(qu.poll());
+		}
+		return list;
+    }
+}
