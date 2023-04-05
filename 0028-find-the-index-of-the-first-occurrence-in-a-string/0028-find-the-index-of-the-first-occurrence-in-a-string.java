0550b68fc7a005e9f6757ae71d2d24058110d3ae
Mahendra Kolhe <mkolhe23@gmail.com>
2023-04-05 23:20:09 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (19.49%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if(haystack.contains(needle))    
+            return haystack.indexOf(needle);   
+        return -1;
+    }
+}
